b4322232d9b3ecb1bfdb79ebb1e62b6bfbdad1a5
Lucas Fernando <lucasfernandosantos2001@gmail.com>
2022-02-24 10:16:48 -0300

    ex4

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import ExUseState1 from "./ExOrigamid/ExUseState1";
 import Ex2UseEffect from "./ExOrigamid/Ex2UseEffect";
 import Ex3UseContext from "./ExOrigamid/Ex3UseContext";
+import Ex4Input from "./ExOrigamid/Ex4Input";
 
 
 function App() {
 
 
   return (
-  <Ex3UseContext />
+  <Ex4Input />
   );
 }
 
