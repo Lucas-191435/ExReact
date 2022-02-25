81c599fd2d25a7e040fc39e57c6a8e6a6c772c94
Lucas Fernando <lucasfernandosantos2001@gmail.com>
2022-02-25 09:52:26 -0300

    ex2

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
 
 
   return (
-  <ExUseState1 />
+  <Ex2UseEffect />
   );
 }
 
--- src/ExOrigamid/Ex2UseEffect.js
+++ src/ExOrigamid/Ex2UseEffect.js
@@ -3,50 +3,51 @@ import React, { useEffect } from 'react';
 
 const Ex2UseEffect = ()=>{
   const [produto, setProduto] = React.useState(null);
-
-  useEffect(()=>{
+  React.useEffect(()=>{
     const produtoLocal = window.localStorage.getItem('produto');
-    if(produtoLocal !== "null") setProduto(produtoLocal);
-  }, []);
+    if(produtoLocal !== null) setProduto(produtoLocal);
+  },[])
 
-  useEffect(()=>{
-    if(produto !== null) window.localStorage.setItem('produto', produto);
-  }, [produto])
+  React.useEffect(()=>{
+    window.localStorage.setItem('produto', produto);
+  }, [produto]);
 
-  function handleClick({target}){
+
+  function handleChange({target}){
     setProduto(target.innerText);
   }
 
-  return (
+  return(
     <div>
-      <button onClick={handleClick}>tablet</button>{' '}
-      <button onClick={handleClick}>smartphone</button>{' '}
-      <button onClick={handleClick}>notebook</button>{' '}
+      <ul>
+        <li><button onClick={handleChange}>tablet</button></li>
+        <li><button onClick={handleChange}>notebook</button></li>
+        <li><button onClick={handleChange}>smartphone</button></li>
+      </ul>
 
       <Produto produto={produto} />
     </div>
   )
 }
 
-export default Ex2UseEffect;
 
 const Produto = ({produto})=>{
   const [dados, setDados] = React.useState(null);
- 
-  useEffect(()=>{
-    if(produto !== null){
-      const url = 'https://ranekapi.origamid.dev/json/api/produto/' + produto;
-      fetch(url).then((response)=>response.json()).then((json)=> setDados(json));
-    }
 
-  }, [produto])
+  React.useEffect(()=>{
+    fetch('https://ranekapi.origamid.dev/json/api/produto/' + produto)
+    .then((response)=> response.json()).then((json)=>setDados(json));
+  },[produto]);
 
-  if (dados === null) return null;
+  if(dados === null) return null;
 
   return(
     <div>
-     <h1>{dados.nome}</h1>
+      <h1>{dados.nome}</h1>
       <p>{dados.preco}</p>
+      <p>{dados.descricao}</p>
     </div>
   )
 }
+
+export default Ex2UseEffect;
